c23416f73ba6abbd2f44d0225be523409870a9a1
Talha <99talhayildiz@gmail.com>
2023-06-28 15:55:16 +0300

    Add project files.

---
+++ Sifreleme(SQL)/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Sifreleme_SQL_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvVeriler = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtHesapNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.BtnListele = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvVeriler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvVeriler
+            // 
+            this.DgvVeriler.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DgvVeriler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvVeriler.Location = new System.Drawing.Point(0, 214);
+            this.DgvVeriler.Name = "DgvVeriler";
+            this.DgvVeriler.RowHeadersWidth = 51;
+            this.DgvVeriler.RowTemplate.Height = 24;
+            this.DgvVeriler.Size = new System.Drawing.Size(876, 290);
+            this.DgvVeriler.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad:";
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(108, 33);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(245, 30);
+            this.TxtAd.TabIndex = 2;
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(108, 69);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(245, 30);
+            this.TxtSoyad.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soyad:";
+            // 
+            // TxtMail
+            // 
+            this.TxtMail.Location = new System.Drawing.Point(108, 105);
+            this.TxtMail.Name = "TxtMail";
+            this.TxtMail.Size = new System.Drawing.Size(245, 30);
+            this.TxtMail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mail:";
+            // 
+            // TxtHesapNo
+            // 
+            this.TxtHesapNo.Location = new System.Drawing.Point(495, 66);
+            this.TxtHesapNo.Name = "TxtHesapNo";
+            this.TxtHesapNo.Size = new System.Drawing.Size(245, 30);
+            this.TxtHesapNo.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(378, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Hesap No:";
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(495, 30);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(245, 30);
+            this.TxtSifre.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(407, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Şifre:";
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(495, 105);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(245, 48);
+            this.BtnKaydet.TabIndex = 11;
+            this.BtnKaydet.Text = "Şifrele Ve Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.Location = new System.Drawing.Point(495, 160);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(245, 48);
+            this.BtnListele.TabIndex = 12;
+            this.BtnListele.Text = "Verileri Listele";
+            this.BtnListele.UseVisualStyleBackColor = true;
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 505);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.TxtHesapNo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DgvVeriler);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvVeriler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvVeriler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtHesapNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Button BtnListele;
+    }
+}
+
---
+++ Sifreleme(SQL)/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Sifreleme_SQL_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-DSQNOEI\SQLEXPRESS03;Initial Catalog=DbProjeler;Integrated Security=True");
+
+        void Listele()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * From TblVeriler", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DgvVeriler.DataSource= dt;
+        }
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            string Ad = TxtAd.Text;
+            byte[] AdDizi = ASCIIEncoding.ASCII.GetBytes(Ad);
+            string SifreliAd = Convert.ToBase64String(AdDizi);
+
+            string Soyad = TxtSoyad.Text;
+            byte[] SoyadDizi = ASCIIEncoding.ASCII.GetBytes(Soyad);
+            string SifreliSoyad = Convert.ToBase64String(SoyadDizi);
+
+            string Mail = TxtMail.Text;
+            byte[] MailDizi = ASCIIEncoding.ASCII.GetBytes(Mail);
+            string SifreliMail = Convert.ToBase64String(MailDizi);
+
+            string Sifre = TxtSifre.Text;
+            byte[] SifreDizi = ASCIIEncoding.ASCII.GetBytes(Sifre);
+            string SifreliSifre = Convert.ToBase64String(SifreDizi);
+
+            string HesapNo = TxtHesapNo.Text;
+            byte[] HesapNoDizi = ASCIIEncoding.ASCII.GetBytes(HesapNo);
+            string SifreliHesapNo = Convert.ToBase64String(HesapNoDizi);
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into TblVeriler (Ad,Soyad,Mail,Sifre,HesapNo) values (@P1,@P2,@P3,@P4,@P5)", baglanti);
+            komut.Parameters.AddWithValue("@P1", SifreliAd);
+            komut.Parameters.AddWithValue("@P2", SifreliSoyad);
+            komut.Parameters.AddWithValue("@P3", SifreliMail);
+            komut.Parameters.AddWithValue("@P4", SifreliSifre);
+            komut.Parameters.AddWithValue("@P5", SifreliHesapNo);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Veriler Şifrelenerek Eklendi");
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            Listele();
+        }
+    }
+}
